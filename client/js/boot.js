3a0c79bc7853b121288e6cd925520f7dd8b36bc6
Frixolydian <mauricio.morales.arias93@gmail.com>
2017-10-07 04:57:27 -0500

    zoom delet

--- client/js/boot.js
+++ client/js/boot.js
@@ -13,6 +13,17 @@ var ASTEROID_LIST = {};
 
 var bootState = {
 	create: function(){
+		if(game.device.desktop) {
+		 
+		// desktop view - desktop code here
+//		game.scale.pageAlignHorizontally = true;
+		//this.scale.setScreenSize(true);
+		 
+		} else {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.setMinMax(480, 320, 720, 480);
+		}
+
 		game.stage.disableVisibilityChange = true;		
 		game.state.start('load');
 	}
