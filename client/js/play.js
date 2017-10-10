18769e8bfbc85c9647dfe90bf3aedc0f39e7612a
Frixolydian <mauricio.morales.arias93@gmail.com>
2017-10-10 00:09:46 -0500

    delete obstacle correctly

--- client/js/play.js
+++ client/js/play.js
@@ -1,7 +1,7 @@
 var playState = {
   create: function(){
 
-    console.log('version pototongo')
+    console.log('version pototaango')
 
     Phaser.Canvas.setImageRenderingCrisp(game.canvas);
     var username = window.location.search.substring(10, window.location.search.length);
